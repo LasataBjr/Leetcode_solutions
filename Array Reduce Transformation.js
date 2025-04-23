3b71682d0dd7d8a7ca082db8ef024b2e55cf2c25
LasataBjr <lasatabjrktm@gmail.com>
2025-04-23 17:37:24 +0545

    Update Leetcode solutions

---
+++ Array Reduce Transformation.js
@@ -0,0 +1,27 @@
+// Given an integer array nums, a reducer function fn, and an initial value init, return the final result obtained 
+// by executing the fn function on each element of the array, sequentially, passing in the return value from the calculation on the preceding element.
+// This result is achieved through the following operations: val = fn(init, nums[0]), val = fn(val, nums[1]), val = fn(val, nums[2]),
+//  ... until every element in the array has been processed. The ultimate value of val is then returned.
+// If the length of the array is 0, the function should return init.
+// Please solve it without using the built-in Array.reduce method.
+
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+    let n = init;
+    for(i=0; i<nums.length; i++){
+        n = fn(n, nums[i]);
+    }
+    return n;
+
+    // let val = init;
+    // for (const n of nums) {
+    //     val = fn(val, n);
+    // }
+    // return val;
+};
+ 
