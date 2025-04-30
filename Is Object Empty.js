70882dde385c3548f6b6f66dd79c8c4ff19b530b
LasataBjr <lasatabjrktm@gmail.com>
2025-04-30 17:27:48 +0545

    update leetcode solution

--- Is Object Empty.js
+++ Is Object Empty.js
@@ -6,13 +6,15 @@ You may assume the object or array is the output of JSON.parse. */
  * @return {boolean}
  */
 var isEmpty = function(obj) {
-    if(JSON.stringify(obj).length <= 2 || Object.keys(obj).length === 0) 
+    if(JSON.stringify(obj).length <= 2 || Object.keys(obj).length === 0) //converting json object into string and using Oject.keys() to obtain to check its length
     {
         return true;
     }else{
         return false;
     }
-    /* for(const key in obj){
+    /* for(const key in obj){   // for in loop for object properties
+                                the interpreter will enter the for-in loop, 
+                                and therefore the first return statement false will be run and if it is empty, the interpreter will not enter the for-in loop
         return false;
     } return true; */
 };
